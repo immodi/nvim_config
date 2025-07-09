7f1457648187fa4c38881f701019d9ff83128b35
immodi <ay727468a@gmail.com>
2025-07-09 15:26:41 +0300

    kotlin treesitter

--- lua/user/plugins/treesitter/treesitter_config.lua
+++ lua/user/plugins/treesitter/treesitter_config.lua
@@ -1,20 +1,21 @@
 local treesitterConfig = require("nvim-treesitter.configs")
 
 treesitterConfig.setup({
-	ensure_installed = {
-		"lua",
-		"python",
-		"html",
-		"templ",
-		"javascript",
-		"typescript",
-		"css",
-		"tsx",
-		"svelte",
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	auto_install = false,
+    ensure_installed = {
+        "lua",
+        "python",
+        "html",
+        "templ",
+        "javascript",
+        "typescript",
+        "css",
+        "tsx",
+        "svelte",
+        "kotlin",
+    },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    auto_install = false,
 })
